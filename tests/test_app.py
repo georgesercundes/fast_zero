1cd496cf0a040821348afbf9dd98136160ef4621
georgesercundes <georgesercundes@gmail.com>
2025-09-27 17:42:28 -0300

    Added first migration using alembic and create users table

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
+from dataclasses import asdict
 from http import HTTPStatus
+
+from sqlalchemy import select
+
+from fast_zero.models import User
 
 
 def test_root_should_return_ok_and_hello_world(client):
@@ -42,7 +47,7 @@ def test_list_users(client):
     }
 
 
-def test_update_user(client):
+def test_update_user_ok(client):
     response = client.put(
         '/users/1',
         json={
@@ -60,8 +65,64 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_update_user_should_return_not_found(client):
+    response = client.put(
+        '/users/666',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_find_user_ok(client):
+    response = client.get('users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_find_user_should_return_not_found(client):
+    response = client.get('users/-1')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user_ok(client):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted!'}
+
+
+def test_delete_user_should_return_not_found(client):
+    response = client.delete('/users/999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_create_user_db(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='alice', password='secret', email='test@test')
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'alice',
+        'password': 'secret',
+        'email': 'test@test',
+        'created_at': time,
+    }
